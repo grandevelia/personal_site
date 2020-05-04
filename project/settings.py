37c17326cfcb9a1606c7c8f12bc8d509362eb833
Elias DeVoe <grandevelia@yahoo.com>
2020-05-04 12:08:51 -0500

    add setup vars to django settings

--- project/settings.py
+++ project/settings.py
@@ -158,3 +158,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
