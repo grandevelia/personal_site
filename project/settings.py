91be479e172ed06a5925fa7bec407b8f60bfee3d
Elias DeVoe <grandevelia@yahoo.com>
2020-05-04 18:29:03 -0500

    test removing django-heroku locals

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import datetime
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,7 +156,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build'),
