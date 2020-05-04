1739022ffd0b537583e903bd5006a88d407a64c0
Elias DeVoe <grandevelia@yahoo.com>
2020-05-04 12:18:26 -0500

    update whitenoise config

--- project/settings.py
+++ project/settings.py
@@ -161,4 +161,4 @@ STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
