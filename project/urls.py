f3cfd386fd83a59ed56c4dc16187aa1d209067da
Elias DeVoe <grandevelia@yahoo.com>
2020-05-04 20:18:15 -0500

    add path

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from personal_site import urls, api, views
 
+from django.urls import path
+
 urlpatterns = [
     url(r'^api/', include(urls)),
     path('', index, name='index'),
