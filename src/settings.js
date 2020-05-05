0195c550d126bfbf07dff9e338549eac754c52b6
Elias DeVoe <grandevelia@yahoo.com>
2020-05-04 20:35:00 -0500

    update api url

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-var apiTarget = 'https://weightscoping.eliadevoe.com/';
+var apiTarget = 'https://devoeelias.herokuapp.com/';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     apiTarget = 'http://127.0.0.1:8000/';
 }
